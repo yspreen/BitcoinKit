2293035453590c4ae4766230944473d8366529d8
Nick Spreen <yannick.spreen@gmail.com>
2024-04-23 23:38:11 -0400

    Move to different openssl lib.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "BitcoinKit", targets: ["BitcoinKit"])
     ],
     dependencies: [
-        .package(url: "https://github.com/vapor-community/copenssl.git", .exact("1.0.0-rc.1")),
+        .package(url: "https://github.com/krzyzanowskim/OpenSSL.git", .upToNextMinor(from: "3.1.5001")),
         .package(url: "https://github.com/Boilertalk/secp256k1.swift", .upToNextMinor(from: "0.1.0")),
         .package(url: "https://github.com/vapor-community/random.git", .upToNextMinor(from: "1.2.0"))
     ],
